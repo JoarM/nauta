790a7de1765e1ab4678c8a25fa7a614a022d482d
JoarM <93974183+JoarM@users.noreply.github.com>
2023-08-13 12:47:59 +0300

    Update Elegance.tsx

--- components/Launchpage/Elegance.tsx
+++ components/Launchpage/Elegance.tsx
@@ -29,7 +29,7 @@ export default function Elegance() {
                             </span>
                             <span className="text-sm py-1 px-2 rounded flex items-center bg-primary-foreground text-primary flex-shrink-0">
                                 <Plus className="w-3 h-3 mr-2"/>
-                                Add
+                                <span className="hidden sm:inline">Add</span>
                             </span>
                         </div>
                         <div className="grid grid-cols-2 mt-3">
