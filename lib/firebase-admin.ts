764f9a7b4e65c17e31f009cc2b59fab687109182
JoarM <joar.maltesson@gmail.com>
2023-08-23 18:57:51 +0200

    parse before

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,10 +1,12 @@
 import { initFirestore } from "@auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
 
+const privateKey = JSON.parse(process.env.FIREBASE_SERVER_PRIVATE_KEY as string);
+
 export const db = initFirestore({
     credential: cert({
         projectId: process.env.FIREBASE_SERVER_PROJECT_ID,
-        privateKey: JSON.parse(process.env.FIREBASE_SERVER_PRIVATE_KEY as string),
+        privateKey: privateKey,
         clientEmail: process.env.FIREBASE_SERVER_CLIENT_EMAIL,
     }),
 });
